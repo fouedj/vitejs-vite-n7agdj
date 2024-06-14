98d5dab7b1767b8bea875acac1f22a32fbf77df7
fouedj <fouedj@hotmail.com>
2024-06-14 13:11:48 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,57 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
+  const [tempCarac, setTempCarac] = useState('');
+  const [tempString, setTempString] = useState('');
   const [carac, setCarac] = useState('');
   const [string, setString] = useState('');
+  const [count, setCount] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+
   const countCharacterOccurrences = (str, char) => {
     return str.split('').reduce((count, currentChar) => {
       return currentChar === char ? count + 1 : count;
     }, 0);
   };
-  const count = countCharacterOccurrences(string, carac);
+
+  const handleCount = () => {
+    setCarac(tempCarac);
+    setString(tempString);
+    const result = countCharacterOccurrences(tempString, tempCarac);
+    setCount(result);
+    setShowResult(true); // Afficher le résultat après le clic
+  };
+
   return (
     <>
       <div>
         Mettez votre mot
         <br />
         <input
-          name="carac"
-          value={string}
-          onChange={(e) => setString(e.target.value)}
+          name="string"
+          value={tempString}
+          onChange={(e) => setTempString(e.target.value)}
         />
         <br />
         <br />
         Mettez le caractère à chercher <br />
         <input
           name="carac"
-          value={carac}
-          onChange={(e) => setCarac(e.target.value)}
+          value={tempCarac}
+          onChange={(e) => setTempCarac(e.target.value)}
           maxLength={1}
         />
         <br />
       </div>
       <div className="card">
-        <button onClick={countCharacterOccurrences}>
-          Le caractère "{carac}" apparaît {count} fois dans la chaîne "{string}"
+        <button onClick={handleCount}>
+          Compter
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {showResult && (
+          <p>Le caractère "{carac}" apparaît {count} fois dans la chaîne "{string}"</p>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
